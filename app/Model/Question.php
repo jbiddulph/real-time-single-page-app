175561f201c80303f15a8a768d8742a70239b7f4
John Biddulph <john.mbiddulph@gmail.com>
2018-10-20 10:37:35 +0100

    set up relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,20 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Question extends Model
 {
-    //
+    // User
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    // Replies
+    public function replies() {
+        return $this->hasMany(Reply::class);
+    }
+    // Category
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
