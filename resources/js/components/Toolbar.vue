6c51da214776cd2a669294937ce7f570aedb5d73
John Biddulph <john.mbiddulph@gmail.com>
2018-12-08 23:14:46 +0000

    Login component and login

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -3,12 +3,18 @@
     <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
     <v-toolbar-title>Pubs UK</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Forum</v-btn>
+    <div class="hidden-sm-and-down">
+      <router-link to="/forum">
+        <v-btn flat>Forum</v-btn>
+      </router-link>
+      
       <v-btn flat>Pubs</v-btn>
       <v-btn flat>Category</v-btn>
-      <v-btn flat>Login</v-btn>
-    </v-toolbar-items>
+      <router-link to="/login">
+        <v-btn flat>Login</v-btn>
+      </router-link>
+      
+    </div>
   </v-toolbar>
 </template>
 
